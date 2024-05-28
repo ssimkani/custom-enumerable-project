df36492741467b2a08f551dd2a5e63f65afed7fe
Seeba184 <seenasimkani@gmail.com>
2024-05-28 07:12:25 -0400

    Add my_each method to Array class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    counter = 0
+    length.times do
+      yield self[counter]
+      counter += 1
+    end
+    self
+  end
 end
