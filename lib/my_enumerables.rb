2981e12062f860d1c21e0c99c7ae743e9f33efef
Seeba184 <seenasimkani@gmail.com>
2024-05-28 07:23:47 -0400

    Add my_each_with_index method to Enumerable class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    counter = 0
+    length.times do
+      yield self[counter], counter
+      counter += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
